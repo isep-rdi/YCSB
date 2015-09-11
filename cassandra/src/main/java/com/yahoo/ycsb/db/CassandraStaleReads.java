07b22e6222271bc2f7fb2df1cb5469f1f60b699b
slvn-lefebvre <slvn-lefebvre@users.noreply.github.com>
2015-09-11 12:08:50 +0200

    update name

--- cassandra/src/main/java/com/yahoo/ycsb/db/CassandraStaleReads.java
+++ cassandra/src/main/java/com/yahoo/ycsb/db/CassandraStaleReads.java
@@ -46,7 +46,7 @@ import org.apache.cassandra.thrift.*;
 /**
  * Cassandra 1.0.6 client for YCSB framework
  */
-public class CassandraClient10 extends DB
+public class CassandraClientStaleReads extends DB
 {
   static Random random = new Random();
   public static final int Ok = 0;
